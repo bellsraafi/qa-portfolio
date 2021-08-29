e4d244201f0a4cefce6889dcec0fe890de51e378
Bello Abdulrafiu <bellsraafi@gmail.com>
2021-08-29 03:02:14 +0100

    commit about page file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import About from './components/About';
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <main className='text-gray-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+    </main>
+  );
+}
+
+/*
+<Projects />
+<Skills />
+<Testimonials />
+<Contact />
+*/
